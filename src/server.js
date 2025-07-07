7c7527a9a27fa124fe1b4436e7b4adbcf0076724
GbFerrera <81236040biel@gmail.com>
2025-07-07 18:02:42 -0300

    refatorando logica para aceitar todas as REQ

--- src/server.js
+++ src/server.js
@@ -8,8 +8,12 @@ const routes = require("./routes")
 
 const app = express()
 
-// Enable CORS for all requests
-app.use(cors())
+// Enable CORS for all origins with all options
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'super_admin_id']
+}))
 
 app.use(express.json())
 app.use(routes)
